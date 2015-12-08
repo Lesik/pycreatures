41ced3c960aa16fa54654874076939c85919bbb2
Oles Pidgornyy <pidgornyy@informatik.uni-frankfurt.de>
2015-12-08 15:30:57 +0100

    add corn class

--- plant.py
+++ plant.py
@@ -3,6 +3,7 @@
 """Docstring
 """
 
+from thing import Thing
 import random
 
 __author__ = "6082200: Oles Pidgornyy, 6040608: Phillip Berger"
@@ -10,7 +11,7 @@ __email__ = "pidgornyy@informatik.uni-frankfurt.de," \
 						"berger.phillip@hotmail.com"
 
 
-class Plant:
+class Plant(Thing):
 
 	def __init__(self):
 		pass
