21686560005cc93a2bdda9ebefcc1b2fd5cd7730
pbkush <pbkush@hotmail.de>
2015-12-08 15:40:02 +0100

    reformated code

--- world.py
+++ world.py
@@ -7,51 +7,53 @@ from thing import Thing
 
 __author__ = "6082200: Oles Pidgornyy, 6040608: Phillip Berger"
 __email__ = "pidgornyy@informatik.uni-frankfurt.de," \
-						"berger.phillip@hotmail.com"
+            "berger.phillip@hotmail.com"
 
 
 class World:
+    world = {}
 
-	world = {}
+    def __init__(self, rows=50, cols=50):
+        self.rows = rows
+        del rows
+        self.cols = cols
+        del cols
+        for row in range(self.rows):
+            for col in range(self.cols):
+                self.world.update({(row, col): None})
 
-	def __init__(self, rows=50, cols=50):
-		self.rows = rows
-		del rows
-		self.cols = cols
-		del cols
-		for row in range(self.rows):
-			for col in range(self.cols):
-				self.world.update({(row, col): None})
+    def rem_thing(self, posx, posy):
+        self.world.update({(posx, posy): None})
 
-	def rem_thing(self, posx, posy):
-		self.world.update({(posx, posy): None})
-
-	def view_coordinate(self, row, col):
-		if self.world[(row, col)] is None:
-			print('.', end='')
-		else:
-			print(self.world[(row, col)], end='')
-
-	def view_world(self):
-		for row in range(self.rows):
-			for col in range(self.cols):
-				self.view_coordinate(row, col)
-			print()
-
-	def add_thing(self, posx, posy):
-		self.world.update({(posx, posy): Thing()})
-
-    def get_pos(self, posx, posy):
-        print(self.world[(posx, posy)])
-
-    def get_cardpoint_pos(self, posx, posy, cardpoint):
-        if cardpoint == 0:
-            if posy == 0:
-                print(self.world[(posx, self.rows)])
-            else:
-                print(self.world[(posx, posy - 1)])
+    def view_coordinate(self, row, col):
+        if self.world[(row, col)] is None:
+            print('.', end='')
         else:
-            pass
+            print(self.world[(row, col)], end='')
+
+    def view_world(self):
+        for row in range(self.rows):
+            for col in range(self.cols):
+                self.view_coordinate(row, col)
+            print()
+
+    def add_thing(self, posx, posy):
+        self.world.update({(posx, posy): Thing()})
+
+
+def get_pos(self, posx, posy):
+    print(self.world[(posx, posy)])
+
+
+def get_cardpoint_pos(self, posx, posy, cardpoint):
+    if cardpoint == 0:
+        if posy == 0:
+            print(self.world[(posx, self.rows)])
+        else:
+            print(self.world[(posx, posy - 1)])
+    else:
+        pass
+
 
 world = World(rows=10, cols=20)
 print("      Empty world:")
