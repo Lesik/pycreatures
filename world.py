358d61f753f29b4992c76ac969835e77f036620a
Oles Pidgornyy <pidgornyy@informatik.uni-frankfurt.de>
2015-12-08 15:27:05 +0100

    Merge branch 'master' of /home/lesik/dev/pycreatures with conflicts.

--- world.py
+++ world.py
@@ -7,48 +7,49 @@ from thing import Thing
 
 __author__ = "6082200: Oles Pidgornyy, 6040608: Phillip Berger"
 __email__ = "pidgornyy@informatik.uni-frankfurt.de," \
-            "berger.phillip@hotmail.com"
+						"berger.phillip@hotmail.com"
 
 
 class World:
-    world = {}
 
-    def __init__(self, rows=50, cols=50):
-        self.rows = rows
-        del rows
-        self.cols = cols
-        del cols
-        for row in range(self.rows):
-            for col in range(self.cols):
-                self.world.update({(row, col): None})
-        pass
+	world = {}
 
-    def rem_thing(self, posx, posy):
-        self.world.update({(posx, posy): None})
+	def __init__(self, rows=50, cols=50):
+		self.rows = rows
+		del rows
+		self.cols = cols
+		del cols
+		for row in range(self.rows):
+			for col in range(self.cols):
+				self.world.update({(row, col): None})
 
-    def view_coordinate(self, row, col):
-        if self.world[(row, col)] is None:
-            print('.', end='')
-        else:
-            print(self.world[(row, col)], end='')
+	def rem_thing(self, posx, posy):
+		self.world.update({(posx, posy): None})
 
-    def view_world(self):
-        for row in range(self.rows):
-            for col in range(self.cols):
-                self.view_coordinate(row, col)
-            print()
+	def view_coordinate(self, row, col):
+		if self.world[(row, col)] is None:
+			print('.', end='')
+		else:
+			print(self.world[(row, col)], end='')
 
-    def add_thing(self, posx, posy):
-        self.world.update({(posx, posy): Thing()})
+	def view_world(self):
+		for row in range(self.rows):
+			for col in range(self.cols):
+				self.view_coordinate(row, col)
+			print()
 
-    def get_pos(self, posx, posy):
-        pos = self.world[(posx, posy)]
-        print(pos)
-
+	def add_thing(self, posx, posy):
+		self.world.update({(posx, posy): Thing()})
 
 world = World(rows=10, cols=20)
-world.add_thing(0, 15)
-world.add_thing(9, 15)
-world.rem_thing(9, 15)
+print("      Empty world:")
 world.view_world()
-world.get_pos(0, 15)
+world.add_thing(0, 15)
+print("      Added Thing at 0, 15:")
+world.view_world()
+world.add_thing(9, 15)
+print("      Added Thing at 9, 15:")
+world.view_world()
+world.rem_thing(9, 15)
+print("      Removed Thing at 9, 15:")
+world.view_world()
