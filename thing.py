955f6ae6417a755a4e53ba3e05ecc147115fb437
Lesik <o.pidgornyy@reply.de>
2019-07-30 09:55:35 +0200

    Obfuscate personal data

--- thing.py
+++ thing.py
@@ -5,9 +5,8 @@
 
 import random
 
-__author__ = "6082200: Oles Pidgornyy, 6040608: Phillip Berger"
-__email__ = "pidgornyy@informatik.uni-frankfurt.de," \
-			"berger.phillip@hotmail.com"
+__author__ = "me and my partner"
+__email__ = "email@university"
 
 
 class Thing:
